ad14d95e1d3033ba341efd846e855fd7bcb8eb8a
Joe Attardi <joe@attardi.net>
2019-05-17 09:55:44 -0400

    add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
+
+ReactGA.initialize('UA-80557105-6');
+ReactGA.pageview('/');
 
 ReactDOM.render(<App />, document.getElementById('root'));
